63204e64b6ea0e04b89c885f06ae77b6f76c3847
dthreshilithadevaraj-maker <dthreshilithaddevaraj@gmail.com>
2026-04-10 21:51:10 +0530

    Update Q04.py

--- Q04.py
+++ Q04.py
@@ -12,3 +12,10 @@
 #   *****
 
 # --- YOUR CODE HERE ---
+n = int(input("Enter number of rows: "))
+
+for i in range(1, n + 1):
+    
+    for j in range(i):
+        print("*", end="")
+    print()  
